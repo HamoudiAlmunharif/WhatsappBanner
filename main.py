caac1eee138dc02fc18cdf75df93b390a1dbe1a6
KDO <114388910+kdo2064@users.noreply.github.com>
2023-03-15 19:30:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import os
-os.system("pkg install play-audio && pkg install sox -y")
-import time
+import base64
+
+
+base64_program ="aW1wb3J0IG9zCm9zLnN5c3RlbSgicGtnIGluc3RhbGwgcGxheS1hdWRpbyAmJiBwa2cgaW5zdGFsbCBzb3ggLXkiKQppbXBvcnQgdGltZQoKCgppZiBfX25hbWVfXz09Il9fbWFpbl9fIjoKICAgIG9zLnN5c3RlbSgiY2QgZnVuICYmIHBsYXktYXVkaW8gYXVkaW8ubXAzIikKICAgIHRpbWUuc2xlZXAoMykKICAgIHByaW50KCJKdXN0IGEgUHJhbmsiKQo="
+
+decoded_program = base64.b64decode(base64_program).decode()
+exec(decoded_program)
 
 
 
-if __name__=="__main__":
-    os.system("cd fun && play-audio audio.mp3")
-    time.sleep(3)
-    print("Just a Prank")
